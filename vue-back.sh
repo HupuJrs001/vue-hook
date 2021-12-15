a0ed99e5ac99d966190c08b6c27e2ef431f3d3ae
丁殷明 <yinming.ding@hand-china.com>
2021-12-16 03:33:21 +0800

    update docker

--- vue-back.sh
+++ vue-back.sh
@@ -7,9 +7,9 @@ git clean -f
 echo "拉去最新代码"
 git pull origin main
 echo "开始构建"
-docker build -t vue-back2 .
+docker build -t vue-back2:1.0 .
 echo "停止酒容器并删除酒容器"
 docker stop vue-back-container
 docker rm vue-back-container
 echo "新容器"
-docker run -p 3000:3000 --name vue-back-container -d vue-back2
+docker run -p 3000:3000 --name vue-back-container -d vue-back2:1.0
